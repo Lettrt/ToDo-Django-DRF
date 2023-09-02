864fb615ca27113ae8459ef72634a4427b3bdb9d
Lettrt <ak555110689@gmail.com>
2023-09-02 22:18:26 +0600

    first commit

---
+++ backend/urls.py
@@ -0,0 +1,8 @@
+'''site_admin'''
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
